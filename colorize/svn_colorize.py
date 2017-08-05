7496dd4bae6013f53738d47e83ab85e145426b06
Kevin Yokley <kyokley2@gmail.com>
2017-08-04 22:27:25 -0500

    Using borderless table

--- colorize/svn_colorize.py
+++ colorize/svn_colorize.py
@@ -2,6 +2,7 @@ import sys
 import re
 from colorclass import Color
 from random import SystemRandom
+from terminaltables import BorderlessTable
 
 COLOR_TAGS = [
         'autored',
@@ -13,23 +14,36 @@ COLOR_TAGS = [
         ]
 SystemRandom().shuffle(COLOR_TAGS)
 
-COMMIT_REGEX = re.compile(r'\s*\d+')
+COMMIT_REGEX = re.compile(r'\s*[0-9a-fA-F]+')
+COMMIT_AUTHOR_REGEX = re.compile(r'^\s*\d+\s+\S+')
+
 commit_dict = dict()
 
 def main():
+    rows = []
     num_authors = 0
 
     for line in sys.stdin.readlines():
         match = COMMIT_REGEX.match(line)
         commit = match.group().strip()
 
+        match = COMMIT_AUTHOR_REGEX.match(line)
+        author = match.group().split()[1]
+        code = line[match.end():]
+
         if commit not in commit_dict:
             commit_dict[commit] = COLOR_TAGS[num_authors % len(COLOR_TAGS)]
             num_authors += 1
 
         tag = commit_dict[commit]
-        print(Color('{%s}%s{/%s}' % (tag, line.strip(), tag)))
+        rows.append([Color('{%s}%s{/%s}' % (tag, commit, tag)),
+                     Color('{%s}%s{/%s}' % (tag, author, tag)),
+                     Color('{%s}%s{/%s}' % (tag, code, tag)),
+                     ])
 
+    table = BorderlessTable(rows)
+    table.inner_heading_row_border = False
+    print(table.table)
 
 if __name__ == '__main__':
     main()
