0284ea6993a27fac0322f7e0c7a0eb7912683553
Kevin Yokley <kyokley2@gmail.com>
2017-08-04 19:26:44 -0500

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Color Blame",
+
+    version="0.1",
+
+    description="Colorize VCS blame command output",
+    long_description="Colorize blame output for git and svn",
+
+    url="https://github.com/kyokley/color_blame",
+
+    author="Kevin Yokley",
+    author_email="kyokley2@gmail.com",
+
+    license="MIT",
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ],
+
+    packages=find_packages(),
+
+    install_requires=[
+        'terminaltables',
+        'colorclass',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose',
+                   'mock',
+                   ],
+
+    entry_points={
+        "console_scripts": [
+            "color_svn_blame = colorize.svn_colorize:main",
+        ],
+    },
+)
