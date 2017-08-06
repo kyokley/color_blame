ee812b8be106eaadb8e38125ab343351a5b028c9
Kevin Yokley <kyokley2@gmail.com>
2017-08-05 21:31:54 -0500

    Fleshing out tests for git

--- colorize/tests/test_git_colorize.py
+++ colorize/tests/test_git_colorize.py
@@ -4,10 +4,25 @@ from colorize.git_colorize import main
 
 class TestMain(unittest.TestCase):
     def setUp(self):
+        self.COLOR_TAGS_patcher = mock.patch('colorize.git_colorize.COLOR_TAGS', ['mock_color'])
+        self.COLOR_TAGS_patcher.start()
+
+        self.Color_patcher = mock.patch('colorize.git_colorize.Color')
+        self.mock_Color = self.Color_patcher.start()
+        self.mock_Color.side_effect = ['colored_commit',
+                                       'colored_author',
+                                       'colored_code']
+
+        self.BorderlessTable_patcher = mock.patch('colorize.git_colorize.BorderlessTable')
+        self.mock_BorderlessTable = self.BorderlessTable_patcher.start()
+
         self.stdin_lines_patcher = mock.patch('colorize.git_colorize.stdin_lines')
         self.mock_stdin_lines = self.stdin_lines_patcher.start()
 
     def tearDown(self):
+        self.COLOR_TAGS_patcher.stop()
+        self.Color_patcher.stop()
+        self.BorderlessTable_patcher.stop()
         self.stdin_lines_patcher.stop()
 
     def test_ascii(self):
@@ -15,12 +30,36 @@ class TestMain(unittest.TestCase):
                 ]
         main()
 
+        self.mock_Color.assert_any_call(u'{mock_color}e7ab9458{/mock_color}')
+        self.mock_Color.assert_any_call(u'{mock_color}(John Doe        2017-05-13 16:24:56 -0700    1){/mock_color}')
+        self.mock_Color.assert_any_call(u'{mock_color} #!/usr/bin/env python{/mock_color}')
+
+        self.mock_BorderlessTable.assert_called_once_with([['colored_commit',
+                                                            'colored_author',
+                                                            'colored_code']])
+
     def test_unicode(self):
         self.mock_stdin_lines.return_value = [u'e7ab9458 (\u0134\xf0\u0127\xf1 \xd0\xf0\xeb        2017-05-13 16:24:56 -0700    1) #!/usr/bin/env python',
                 ]
         main()
 
+        self.mock_Color.assert_any_call(u'{mock_color}e7ab9458{/mock_color}')
+        self.mock_Color.assert_any_call(u'{mock_color}(\u0134\xf0\u0127\xf1 \xd0\xf0\xeb        2017-05-13 16:24:56 -0700    1){/mock_color}')
+        self.mock_Color.assert_any_call(u'{mock_color} #!/usr/bin/env python{/mock_color}')
+
+        self.mock_BorderlessTable.assert_called_once_with([['colored_commit',
+                                                            'colored_author',
+                                                            'colored_code']])
+
     def test_byte_str(self):
         self.mock_stdin_lines.return_value = [u'e7ab9458 (\u0134\xf0\u0127\xf1 \xd0\xf0\xeb        2017-05-13 16:24:56 -0700    1) #!/usr/bin/env python'.encode('utf-8'),
                 ]
         main()
+
+        self.mock_Color.assert_any_call(u'{mock_color}e7ab9458{/mock_color}')
+        self.mock_Color.assert_any_call(u'{mock_color}(\u0134\xf0\u0127\xf1 \xd0\xf0\xeb        2017-05-13 16:24:56 -0700    1){/mock_color}')
+        self.mock_Color.assert_any_call(u'{mock_color} #!/usr/bin/env python{/mock_color}')
+
+        self.mock_BorderlessTable.assert_called_once_with([['colored_commit',
+                                                            'colored_author',
+                                                            'colored_code']])
